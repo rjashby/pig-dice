206dcd6443f58ece248f3fe019ee6653dd23f1bf
Ryan Ashby <rjapsu160@gmail.com>
2022-02-01 10:10:21 -0800

    add environment, and write first test in jest/pass
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>

---
+++ _tests_/pigdice.test.js
@@ -0,0 +1,13 @@
+import Dice from "./../src/js/dice.js";
+
+describe("Dice", () => {
+  test('should create a dice object with score, rolls, turns, and totals properties', () => {
+    let myDice = new Dice (0, [], true, false, 0, 0);
+    expect(myDice.scoreCount).toEqual(0);
+    expect(myDice.rolls).toEqual([]);
+    expect(myDice.p1Turn).toEqual(true);
+    expect(myDice.p2Turn).toEqual(false);
+    expect(myDice.p1Total).toEqual(0);
+    expect(myDice.p2Total).toEqual(0);
+  });
+});
---
+++ src/js/dice.js
@@ -0,0 +1,66 @@
+export default class Dice {
+  constructor(scoreCount, rolls, p1Turn, p2Turn, p1Total, p2Total) {
+  this.scoreCount = 0;
+  this.rolls = [];
+  this.p1Turn = true;
+  this.p2Turn = false;
+  this.p1Total = 0;
+  this.p2Total = 0;
+}
+
+  diceRoll = function() {
+    let rollOfDice =  Math.floor(Math.random() * 6 ) + 1;
+    return rollOfDice;
+  }
+
+  diceCount = function() {
+    let myRoll = this.diceRoll();
+    console.log(myRoll);
+    updateImage(myRoll);
+    if (myRoll === 1) {
+      this.scoreCount = 0;
+      this.rolls = [];
+      this.p1Turn = !this.p1Turn;
+      this.p2Turn = !this.p2Turn;
+    } else if (myRoll !== 1) {
+      if (this.p1Turn === true) {
+        this.scoreCount += myRoll;
+        this.rolls.push(myRoll);
+      } else {
+        this.scoreCount += myRoll;
+        this.rolls.push(myRoll);
+      }
+    console.log(this);
+    console.log(this.scoreCount);
+    this.winCondition();
+    }
+  }
+
+  winCondition = function () {
+    if (this.p1Total + this.scoreCount >= 50) {
+      this.p1Total += this.scoreCount;
+      console.log("Player 1 Wins!");
+      $("#outcome").text("Player One Wins!");
+      $(".container").hide();
+    } else if (this.p2Total + this.scoreCount >= 50) {
+      this.p2Total += this.scoreCount;
+      console.log("Player 2 Wins!");
+      $("#outcome").text("Player Two Wins!");
+      $(".container").hide();
+    } else {
+      console.log("Keep playing!");
+    }
+  }
+
+  hold = function() {
+    if (this.p1Turn === true) {
+      this.p1Total += this.scoreCount;
+    } else {  
+      this.p2Total += this.scoreCount;
+    }
+    this.p1Turn = !this.p1Turn;
+    this.p2Turn = !this.p2Turn;
+    this.rolls = [];
+    this.scoreCount = 0;
+  }
+}
